222e061447d4bdf2d551cecd5c0725b61de1a789
Abhinav Kumar <abhinavkr391@gmail.com>
2024-08-12 00:55:30 +0530

    10th August 2024-2

---
+++ src/ex_10082024/Lab008.py
@@ -0,0 +1,13 @@
+# Dynamically typed
+# At the run time, data of the variable can be changed. and you don't need to
+# mention the data type - Python - Int is very smart
+
+age = 45
+print(type(age))
+age = "Pramod"
+print(age)
+print(type(age))
+
+print ("--------")
+long_var_name_is_created_here = "Hello"
+print(long_var_name_is_created_here)
